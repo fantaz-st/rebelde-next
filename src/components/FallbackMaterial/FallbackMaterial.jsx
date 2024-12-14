44e220e0b63b65d983a52365dd0200d2b77113a2
fantaz-st <cbabic@pfst.hr>
2024-12-14 15:57:48 +0100

    Release 1

--- src/components/FallbackMaterial/FallbackMaterial.jsx
+++
@@ -1,8 +0,0 @@
-import { useTexture } from "@react-three/drei";
-
-const FallbackMaterial = ({ url }) => {
-  const texture = useTexture(url);
-  return <meshBasicMaterial map={texture} toneMapped={false} />;
-};
-
-export default FallbackMaterial;
--- src/components/Hero2/Menu/Menu.jsx
+++
@@ -1,7 +0,0 @@
-import classes from "./Menu.module.css";
-
-const Menu = () => {
-  return <div className={classes.container}>menu</div>;
-};
-
-export default Menu;
